8243968ddbeb0ea2066404841253a72167ebfd76
Angad1141 <angadpatil1141@gmail.com>
2025-12-14 19:11:55 +0530

    Home and About page updated

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,7 @@ import Beams from "../ui_components/Beams";
 import LogoLoop from "../ui_components/LoopLoop";
 import GlareHover from "../ui_components/GlareHover";
 import SplitText from "../ui_components/SplitText";
+import RotatingText from "../ui_components/RotatingText";
 import {
   SiReact,
   SiNextdotjs,
@@ -49,7 +50,7 @@ const Home = ({ onContactClick }) => {
             beamHeight={30}
             beamNumber={20}
             lightColor="#155dfc"
-            speed={4}
+            speed={6}
             noiseIntensity={1.75}
             scale={0.1}
             rotation={30}
@@ -63,7 +64,7 @@ const Home = ({ onContactClick }) => {
             <h4 className="text-lg lg:text-3xl">Hi, I'm Angad Patil</h4>
             <SplitText
               text="Software Developer"
-              className="mt-3 md:mt-5 tracking-wide md:tracking-normal text-6xl lg:text-8xl font-bold"
+              className="mt-3 md:mt-5 tracking-wide md:tracking-normal text-7xl lg:text-8xl font-black"
               delay={100}
               duration={0.6}
               ease="power3.out"
@@ -75,7 +76,22 @@ const Home = ({ onContactClick }) => {
               textAlign="center"
               onLetterAnimationComplete={handleAnimationComplete}
             />
-            <div className="mt-5 flex justify-center items-center h-10 lg:h-20 gap-5">
+            <div className="mt-5 md:mt-5 w-full flex justify-center items-center gap-3 text-2xl md:text-4xl font-black">
+              <h1>Creative</h1>
+              <RotatingText
+                texts={["Coding", "Thinking", "Components!"]}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-[#155dfc] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </div>
+            <div className="mt-7 md:mt-5 flex justify-center items-center h-10 lg:h-20 gap-5">
               <GlareHover
                 glareColor="#ffffff"
                 glareOpacity={0.3}
@@ -149,10 +165,10 @@ const Home = ({ onContactClick }) => {
             </button>
           </div>
         </div>
-        <div className="relative -bottom-36 max-w-screen h-20 flex md:hidden xl:flex items-center overflow-hidden">
+        <div className="relative -bottom-30 max-w-screen h-30 flex md:hidden xl:flex items-center overflow-hidden">
           <LogoLoop
             logos={techLogos}
-            speed={120}
+            speed={100}
             direction="left"
             logoHeight={40}
             gap={40}
